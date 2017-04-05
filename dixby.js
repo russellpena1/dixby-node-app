5490fc68b6bed1ea55487a3031f9dc846124172c
Russell Pena <russellpena@Russells-MacBook-Pro.local>
2017-04-05 18:32:03 -0400

    created all files

---
+++ dixby.js
@@ -0,0 +1,1 @@
+var twitterAPI = require("./keys.js");
---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'QyO3QIdcQHEiWq6Y1EC4kXeJs',
+  consumer_secret: '4EAAmTUX3nK5ldxvRuc9FOEc0HTUFhZXLCEGqWMkkbwJlV2eWv',
+  access_token_key: '2575164437-uDIZdc1Ut63T0SQcSZIgwAlzRCla9kuck8ye0Oz',
+  access_token_secret: 'JQwIwxvzAthbGti6etcy74tcAJgFt5hfeyVVP2jdPDMmp',
+}
+
