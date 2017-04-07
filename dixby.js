330930d61c1eab31edf611cd47916c410c7607d0
Russell Pena <russellpena@Russells-MacBook-Pro.local>
2017-04-07 18:35:11 -0400

    added Twitter API key

--- dixby.js
+++ dixby.js
@@ -1,1 +1,12 @@
+///creating variables for my 'require' queries
 var twitterAPI = require("./keys.js");
+
+var spotify = require("spotify");
+
+var request = require("request");
+
+var TwitterKeyCall = twitterAPI.twitterKeys;
+
+console.log(TwitterKeyCall);
+
+
--- keys.js
+++ keys.js
@@ -1,9 +1,13 @@
 console.log('this is loaded');
 
-exports.twitterKeys = {
+var TwitterPackage = require('twitter');
+
+exports.twitterKeys = new TwitterPackage({
+
   consumer_key: 'QyO3QIdcQHEiWq6Y1EC4kXeJs',
   consumer_secret: '4EAAmTUX3nK5ldxvRuc9FOEc0HTUFhZXLCEGqWMkkbwJlV2eWv',
   access_token_key: '2575164437-uDIZdc1Ut63T0SQcSZIgwAlzRCla9kuck8ye0Oz',
   access_token_secret: 'JQwIwxvzAthbGti6etcy74tcAJgFt5hfeyVVP2jdPDMmp',
-}
+
+});
 
